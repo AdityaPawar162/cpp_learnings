f537a5a0153bb8ff05bf18b925ec2c1670fa7580
Aditya Pawar <adityavpawar2003@gmail.com>
2024-03-12 19:33:02 +0530

    Added Stack folder containing code for stack

--- Stack/Implementation_of_stack_using_linkedList.cpp
+++ Stack/Implementation_of_stack_using_linkedList.cpp
@@ -1,61 +1,60 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <limits.h>
 using namespace std;
-//int top;
-  	struct Node{
-		int data;
-		Node*next;
-	};
-		
-	private:
-		int head;
-	Node*head;	
-	public:
-	
-	void Push(int x)
-	{
-		Node*newNode = new Node;
-		newNode->data = x;
-		newNode->next = head;
-		head = newNode;
-	}
-	void Pop(){
-		Node*temp;
-		if(head == NULL)return;
-		temp = top;
-		top = top->next;
-		free(temp);
-	}
-	
-	int top(){
-		return (*head);
-	}
-	void isEmpty(){
-		if(head != NULL)return 0;
-		else {
-			return 1;
-		}
-	}
-	
-	void Print(){
-		Node*temp = head;
-		cout << "The stack: ";
-		while(temp->next != NULL){
-			temp = temp->next;
-			cout << temp->data;
-		}
-		cout << "/n";
-		
-	}
+
+struct Node {
+    int data;
+    Node* next;
 };
-int main(){
-	stack s;
-	s.Push(2);s.Print();
-	s.Push(4);s.Print();
-	s.Push(6);s.Print();
-	s.Pop();s.Print();
-	cout << "is empty condition: ";
-	s.isEmpty();
-	cout << "value at top";
-	s.top();
-	
+
+class Stack {
+private:
+    Node* head;
+public:
+    Stack() : head(nullptr) {} // Constructor to initialize head to nullptr
+
+    void Push(int x) {
+        Node* newNode = new Node;
+        newNode->data = x;
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void Pop() {
+        if (head == nullptr) return; // Check if stack is empty
+        Node* temp = head;
+        head = head->next;
+        delete temp; // Deallocate memory for popped node
+    }
+
+    int Top() {
+        if (head == nullptr) return INT_MIN; // Assuming INT_MIN represents empty stack
+        return head->data;
+    }
+
+    bool IsEmpty() {
+        return head == nullptr;
+    }
+
+    void Print() {
+        Node* temp = head;
+        cout << "The stack: ";
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Stack s;
+    s.Push(2); s.Print();
+    s.Push(4); s.Print();
+    s.Push(6); s.Print();
+    s.Pop(); s.Print();
+    cout << "Is empty condition: " << s.IsEmpty() << endl;
+    cout << "Value at top: " << s.Top() << endl;
+
+    return 0;
 }
--- Stack/infix_to_postfix.cpp
+++ Stack/infix_to_postfix.cpp
@@ -1,27 +1,35 @@
 #include<iostream>
 #include<stack>
-#include<cstring>
+#include<string>
 using namespace std;
-int Perform(int*exp[i],int*Op1,int*Op2){
-	if((*exp[i]) == '+')return 
+
+int Perform(int Op1, int Op2, char op) {
+    switch(op) {
+        case '+': return Op1 + Op2;
+        case '-': return Op1 - Op2;
+        case '*': return Op1 * Op2;
+        case '/': return Op1 / Op2;
+        default: return 0; // Handle invalid operator
+    }
 }
-int EvaluatePostfix(string exp){
-	stack<int> S;
-	for (int i = 0;i < strlen(exp)-1;i++){
-		if(exp[i] != '/'|| exp[i] != '*'||exp[i] != '-'||exp[i] != '+'){
-		
-		S.push(exp[i]);
-	}
-	    else {
-	    	int Op1,Op2;
-	    	Op2 = S.pop();
-	    	Op1 = S.pop();
-	    	res = Perform(&exp[i],&Op2,&Op1);
-	    	S.push(res);
-		}
-	}
-	return S.top()
+
+int EvaluatePostfix(string exp) {
+    stack<int> S;
+    for (int i = 0; i < exp.length(); i++) {
+        if (isdigit(exp[i])) {
+            S.push(exp[i] - '0'); // Convert char digit to int
+        } else if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') {
+            int Op2 = S.top(); S.pop();
+            int Op1 = S.top(); S.pop();
+            int res = Perform(Op1, Op2, exp[i]);
+            S.push(res);
+        }
+    }
+    return S.top();
 }
-int main(){
-	return 0;
+
+int main() {
+    string exp = "42+5*";
+    cout << "Result: " << EvaluatePostfix(exp) << endl;
+    return 0;
 }
