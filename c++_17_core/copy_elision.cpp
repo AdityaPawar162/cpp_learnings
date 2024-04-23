276cba2abd59c71b44431220ae42ba1a21bdbd12
Aditya Pawar <adityavpawar2003@gmail.com>
2024-04-23 09:33:16 +0530

    Added C++_17_core folder containing codes related to C++_17_Core_Language_features

---
+++ c++_17_core/copy_elision.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+class Number{
+    public:
+    Number([[maybe_unused]] int value)
+    {
+        std::cout << "Parameterized ctor\n";
+    }
+    Number(const Number &)
+    {
+        std::cout << "Copy ctor\n";
+    }
+    Number(Number &&)
+    {
+        std::cout << "Move ctor\n";
+    }
+    
+};
+
+void Foo([[maybe_unused]] Number n)
+{
+    std::cout << "Foo\n";
+}
+
+Number CreateNumber()
+{
+    // Number n{10};
+
+    // return n;
+
+    return Number{10};
+}
+
+int main()
+{
+    [[maybe_unused]] Number n1 = Number{3};
+    Foo(Number{3});
+
+
+    [[maybe_unused]] auto n2 = CreateNumber();
+
+    return 0;
+
+}
+
