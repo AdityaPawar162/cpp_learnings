d5922c70c2e172be13c3e5a48de4f4bb25f993bf
Aditya Pawar <adityavpawar2003@gmail.com>
2024-05-03 21:54:20 +0530

    Added C++_17_core folder containing codes related to C++_17_Core_Language_features

--- c++_17_core/if_constexpr.cpp
+++ c++_17_core/if_constexpr.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <type_traits>
 
-template <typename T>
+template<typename T>
 void PrintType()
 {
-    if constexpr(std::is_integral_v<T>)
+    if constexpr( std::is_integral_v<T> )
     {
         std::cout << "Integral type\n";
     }
-    else if constexpr(std::is_floating_point_v<T>)
+    else if constexpr( std::is_floating_point_v<T> )
     {
         std::cout << "Floating point type\n";
     }
-    else if constexpr(std::is_pointer_v<T>)
+    else if constexpr( std::is_pointer_v<T> )
     {
         std::cout << "Pointer type\n";
     }
@@ -22,15 +22,16 @@ void PrintType()
     }
 }
 
-template <typename T>
-void Print(const T &value){
-    if constexpr (std::is_pointer_v<T>)
+template<typename T>
+void Print( const T & value )
+{
+    if constexpr( std::is_pointer_v<T> )
     {
         std::cout << *value << "\n";
     }
-    else if constexpr (std::is_array_v<T>)
+    else if constexpr( std::is_array_v<T> )
     {
-        for(const auto &element : value)
+        for( const auto & element : value )
         {
             std::cout << element << " ";
         }
@@ -50,8 +51,8 @@ int main()
     // PrintType<std::string>();
 
     int value = 5;
-    int *ptr = &value;
-    Print(ptr);
+    int * ptr = &value;
+    Print( ptr );
 
     return 0;
 }
