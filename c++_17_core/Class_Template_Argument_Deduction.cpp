fe689f6c35b804518e942d4e68994d265b777ba8
Aditya Pawar <adityavpawar2003@gmail.com>
2024-05-04 08:31:30 +0530

    Added C++_17_core folder containing codes related to C++_17_Core_Language_features

--- c++_17_core/Class_Template_Argument_Deduction.cpp
+++ c++_17_core/Class_Template_Argument_Deduction.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 template<typename T>
-class Data{
-    public:
-    Data(const T&){}
+class Data
+{
+public:
+    Data( const T & ) {}
 };
-Data(const char*) -> Data<std::string>; // Deduction guide
+Data( const char * )->Data<std::string>; // Deduction guide
 
-int main(){
-    Data data1(10); // T is deduced as int
-    Data d2{8.2f}; // T is explicitly specified as float
-    Data d3{"Hello"}; // T is deduced as const char[6] but the deduction guide specifies that it should be std::string
+int main()
+{
+    Data data1( 10 );   // T is deduced as int
+    Data d2{ 8.2f };    // T is explicitly specified as float
+    Data d3{ "Hello" }; // T is deduced as const char[6] but the deduction guide specifies that it should be std::string
 
     return 0;
 }
