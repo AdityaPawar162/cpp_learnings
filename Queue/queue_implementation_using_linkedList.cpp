18c8f9e64ceb99fd38bb978343f42ab3f95d752c
Aditya Pawar <adityavpawar2003@gmail.com>
2024-05-24 15:14:25 +0530

    Added Queue folder containing codes related to Queues

--- Queue/queue_implementation_using_linkedList.cpp
+++ Queue/queue_implementation_using_linkedList.cpp
@@ -1,59 +1,72 @@
-#include<iostream>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <iostream>
 using namespace std;
-struct Node{
-	int data;
-	Node*next;
+struct Node
+{
+    int data;
+    Node * next;
 };
-Node*front = NULL;
-Node*rear = NULL;
-void Enqueue(int x){
-	Node*newNode = new Node;
-	newNode->data = x;
-	newNode->next = NULL;
-	if(front == NULL && rear == NULL){
-		front = rear = newNode;
-		return;
-	}
-	rear->next = newNode;
-	rear = newNode;
-	
-}
-void Dequeue(){
-	Node*temp = front;
-	if(front == rear ){
-		front = rear = NULL;
+Node * front = NULL;
+Node * rear  = NULL;
+void Enqueue( int x )
+{
+    Node * newNode = new Node;
+    newNode->data  = x;
+    newNode->next  = NULL;
+    if( front == NULL && rear == NULL )
+    {
+        front = rear = newNode;
+        return;
     }
-    if(front == NULL){
-    	cout << "Queue is empty\n";
-    	return;
-	}
-	else{
-		front = front->next;
-	}
-	free(temp);
+    rear->next = newNode;
+    rear       = newNode;
+}
+void Dequeue()
+{
+    Node * temp = front;
+    if( front == rear )
+    {
+        front = rear = NULL;
+    }
+    if( front == NULL )
+    {
+        cout << "Queue is empty\n";
+        return;
+    }
+    else
+    {
+        front = front->next;
+    }
+    free( temp );
+}
+int Front()
+{
+    if( front == NULL )
+    {
+        cout << "Queue is empty\n";
+        return 0;
+    }
+    return front->data;
+}
+void Print()
+{
+    Node * temp = front;
+    while( temp != NULL )
+    {
+        cout << temp->data;
+        temp = temp->next;
+    }
+    cout << "\n";
+}
+int main()
+{
+    Enqueue( 2 );
+    Print();
+    Enqueue( 4 );
+    Print();
+    Enqueue( 6 );
+    Print();
 
-}
-int Front(){
-	if(front == NULL){
-		cout << "Queue is empty\n";
-		return 0;
-		
-	}
-	return front->data;
-}
-void Print(){
-	Node*temp = front;
-	while(temp != NULL){
-		cout << temp->data;
-		temp = temp->next;
-	}
-	cout << "\n";
-}
-int main(){
-		Enqueue(2); Print(); 
-	Enqueue(4); Print();
-	Enqueue(6); Print();
-	
-	Enqueue(8); Print();
+    Enqueue( 8 );
+    Print();
 }
